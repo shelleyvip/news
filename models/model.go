8b37465a5f15118d3e4c38c5439d1ab99049b496
golang <golang@golangdeMacBook-Pro.local>
2019-01-17 12:04:15 +0800

    表设计

--- models/model.go
+++ models/model.go
@@ -11,6 +11,8 @@ type User struct {
 	Name string
 	Pwd string
 	//Age string
+
+	Article[]*Article `orm:"rel(m2m)"`//设置多对多
 }
 
 type Article struct {
@@ -20,15 +22,25 @@ type Article struct {
 	Acount int `orm:"default(0);null"`
 	Acontent string
 	Aimg string
-}
+	// n 记住1的一方
+	ArticleType *ArticleType `orm:"rel(fk)"` //设置一对多的关系
 
+	User[]*User `orm:"reverse(many)"` //设置多对多反向关系
+
+}
+//类型表
+type ArticleType struct {
+	Id int
+	Tname string
+	Article []*Article `orm:"reverse(many)"`  //设置一对多的反向关系
+}
 
 
 func init()  {
 	//设置数据库的基本信息
 	orm.RegisterDataBase("default","mysql","root:123@tcp(127.0.0.1:3306)/test5?charset=utf8")
 	//映射modle数据
-	orm.RegisterModel(new(User),new(Article))
+	orm.RegisterModel(new(User),new(Article),new(ArticleType))
 	//生成表
 	orm.RunSyncdb("default",false,true)
 }
